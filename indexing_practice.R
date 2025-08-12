aa7cd58b306c0ba0e7577302b35e35a470c076e2
avarobillard <avamorganr@gmail.com>
2025-08-12 14:47:53 -0700

    subsetting practice

--- indexing_practice.R
+++ indexing_practice.R
@@ -95,3 +95,45 @@ fruit[,2]
 # mutable 
 fruit[2,1] <- "pineapple"
 fruit
+
+# Select multiple elements
+x <- 1:10
+x
+x[c(3,1,5)]
+x[c(1,1,1)]
+x[c(1.2,5.4,7.2)]
+
+# put into order
+x[order(x)]
+
+# exclude elements with negative integers- can't mix neg and pos
+x[-10]
+x[-c(2,4,6,8,10)]
+
+# logical vectors- data filtering!
+x[c(TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE)]
+x[x>3]
+
+# Subsetting and assigning multiple values- aka removing rows
+x <- 1:5
+x[c(1,2)] <- 2:3
+
+# replace all but first element
+x[-1] <- 4:1
+
+# Subset rows in data frames based on conditions (logical subsetting)
+# ask for gears col with value 5 and all rows
+mtcars[mtcars$gear == 5,]
+
+# multiple columns 
+mtcars[mtcars$gear == 5 & mtcars$cyl == 4, ]
+
+# using subset function
+subset(mtcars, gear == 5 & cyl == 4, )
+
+# Removing columns from data frames
+df <- data.frame(x = 1:3, y = 3:1, z = letters[1:3])
+df
+df$z <- NULL
+df[c("x", "y")]
+df[setdiff(names(df), "z")]
